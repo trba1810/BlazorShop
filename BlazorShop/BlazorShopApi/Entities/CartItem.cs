f834aa7e04a478c4767e9a4f182372165e6b2d63
trba1810 <trba8l@live.com>
2023-05-19 20:50:53 +0200

    added entities

--- BlazorShop/BlazorShopApi/Entities/CartItem.cs
+++ BlazorShop/BlazorShopApi/Entities/CartItem.cs
@@ -2,5 +2,10 @@
 {
     public class CartItem
     {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
     }
 }
--- BlazorShop/BlazorShopApi/Entities/ProductCategory.cs
+++ BlazorShop/BlazorShopApi/Entities/ProductCategory.cs
@@ -2,5 +2,8 @@
 {
     public class ProductCategory
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
     }
 }
