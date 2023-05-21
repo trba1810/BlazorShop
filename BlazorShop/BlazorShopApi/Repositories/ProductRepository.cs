04146fa32870486670c3ea2264f2d4723a67f86a
trba1810 <trba8l@live.com>
2023-05-21 18:12:09 +0200

    added DTO and Interface

---
+++ BlazorShop/BlazorShopApi/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using BlazorShopApi.Entities;
+using BlazorShopApi.Repositories.Contracts;
+
+namespace BlazorShopApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
