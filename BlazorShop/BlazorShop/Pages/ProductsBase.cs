a6727fb4d768c6321f7f2359d932d06e71fa33f3
trba1810 <trba8l@live.com>
2023-05-24 23:04:28 +0200

    added DI for services

---
+++ BlazorShop/BlazorShop/Pages/ProductsBase.cs
@@ -0,0 +1,11 @@
+﻿using BlazorShop.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorShop.Pages
+{
+    public class ProductsBase:ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+    }
+}
--- BlazorShop/BlazorShop/Program.cs
+++ BlazorShop/BlazorShop/Program.cs
@@ -1,4 +1,6 @@
 using BlazorShop;
+using BlazorShop.Services;
+using BlazorShop.Services.Contracts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44313/") });
+builder.Services.AddScoped <IProductService,ProductService>();
 
 await builder.Build().RunAsync();
