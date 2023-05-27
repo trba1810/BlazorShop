178cb836c950a641d07bb31f77b205e62a93d32d
trba1810 <trba8l@live.com>
2023-05-27 05:50:57 +0200

    finish product details base

---
+++ BlazorShop/BlazorShop/Pages/ProductDetailsBase.cs
@@ -0,0 +1,32 @@
+﻿using BlazorShop.Services.Contracts;
+using BlazorShopModels.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorShop.Pages
+{
+    public class ProductDetailsBase:ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public ProductDTO Product { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Product = await ProductService.GetItem(Id);
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
