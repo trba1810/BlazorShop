9cd21f4b620fb65bc1ea49cec65bb81d21e8cd32
trba1810 <trba8l@live.com>
2023-05-28 21:09:56 +0200

    added method to check if cart item exist

--- BlazorShop/BlazorShopApi/Repositories/ShoppingCartRepository.cs
+++ BlazorShop/BlazorShopApi/Repositories/ShoppingCartRepository.cs
@@ -1,6 +1,69 @@
-﻿namespace BlazorShopApi.Repositories
+﻿using BlazorShopApi.Data;
+using BlazorShopApi.Entities;
+using BlazorShopApi.Repositories.Contracts;
+using BlazorShopModels.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorShopApi.Repositories
 {
-    public class ShoppingCartRepository
+    public class ShoppingCartRepository : IShoppingCartRepository
     {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ShoppingCartRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        private async Task<bool> CartItemExists(int cartId,int productId)
+        {
+            return await this.shopOnlineDbContext.CartItems.AnyAsync(c=>c.CartId == cartId && c.ProductId == productId);
+
+        }
+
+        public async Task<CartItem> AddItem(CartItemToAddDTO cartItemToAddDTO)
+        {
+            if(await CartItemExists(cartItemToAddDTO.CartId, cartItemToAddDTO.ProductId)== false)
+            {
+                var item = await (from product in this.shopOnlineDbContext.Products
+                                  where product.Id == cartItemToAddDTO.ProductId
+                                  select new CartItem
+                                  {
+                                      CartId = cartItemToAddDTO.CartId,
+                                      ProductId = product.Id,
+                                      Quantity = cartItemToAddDTO.Quantity,
+                                  }).SingleOrDefaultAsync();
+
+                if (item != null)
+                {
+                    var result = await this.shopOnlineDbContext.AddAsync(item);
+                    await this.shopOnlineDbContext.SaveChangesAsync();
+                    return result.Entity;
+                }
+            }
+            
+
+            return null;
+        }
+
+        public Task<CartItem> DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> GetItems(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> UpdateQuantity(int id, CartItemQtyUpdateDTO cartItemQtyUpdateDTO)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
