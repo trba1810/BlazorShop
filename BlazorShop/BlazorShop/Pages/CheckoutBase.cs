952f3aa726b971c95c7799b4fb55440772978136
trba1810 <trba8l@live.com>
2023-06-07 06:43:20 +0200

    Integration of PayPal payment gateway

--- BlazorShop/BlazorShop/Pages/CheckoutBase.cs
+++ BlazorShop/BlazorShop/Pages/CheckoutBase.cs
@@ -1,6 +1,67 @@
-﻿namespace BlazorShop.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using BlazorShopModels.DTOs;
+using BlazorShop.Services.Contracts;
+using BlazorShop;
+
+namespace BlazorShop.Pages
 {
-    public class CheckoutBase
+    public class CheckoutBase : ComponentBase
     {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+
+        protected IEnumerable<CartItemDTO> ShoppingCartItems { get; set; }
+
+        protected int TotalQty { get; set; }
+
+        protected string PaymentDescription { get; set; }
+
+        protected decimal PaymentAmount { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+                if (ShoppingCartItems != null)
+                {
+                    Guid orderGuid = Guid.NewGuid();
+
+                    PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
+                    TotalQty = ShoppingCartItems.Sum(p => p.Quantity);
+                    PaymentDescription = $"O_{HardCoded.UserId}_{orderGuid}";
+
+                }
+
+            }
+            catch (Exception)
+            {
+                //Log exception
+                throw;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                if (firstRender)
+                {
+                    await Js.InvokeVoidAsync("initPayPalButton");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
     }
 }
